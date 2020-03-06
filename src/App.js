e864d4e4cea4cf0ab5476de09ef7b3fd77c18344
Diego Gomez <diegogomez@Diegos-MacBook-Pro.local>
2020-03-06 00:29:33 -0500

    add menu and range date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.css';
-import PropTypes from 'prop-types';
+import Menu from './Menu/Menu';
+import { Tab, Tabs } from 'react-bootstrap'
+import { DateRange } from 'react-date-range';
+import { moment } from 'moment';
 import Test from './Api/Test';
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
@@ -9,10 +12,44 @@ class App extends Component {
   //   console.log(await Test.getDataTest());
   // }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectionRange : {
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+      }
+    }
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(ranges){
+    console.log(ranges);
+    this.setState({
+      selectionRange: {
+        startDate: ranges.selection.startDate,
+        endDate: ranges.selection.endDate,
+        key: 'selection',
+      }
+    });
+  }
+
   render() {
     return (
       <div>
-        
+        <Menu></Menu>
+        <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+          <Tab eventKey="profile" title="Por Consultor">
+          <DateRange
+            ranges={[this.state.selectionRange]}
+            onChange={this.handleSelect}
+          />
+          </Tab>
+          <Tab eventKey="contact" title="Por Cliente" disabled>
+            <h1>Hola</h1>
+          </Tab>
+        </Tabs>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 import App from './App';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
